b5b5371b532cc1eb945df8f50b524532f3554e83
mreza <mrezabmiri17@gmail.com>
2023-07-31 19:58:05 +0330

    Content&Navbar Components Added But Not Completed.

---
+++ src/components/Content.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+const Content = () => {
+  return (
+    <div className="w-screen h-screen bg-yellow-400 ">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Content;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <div className="w-screen h-20 bg-pink-400  "></div>;
+};
+
+export default Navbar;
