c441130d9b59c161cb79d489a6cb1721fbff5fb4
mreza <mrezabmiri17@gmail.com>
2023-08-01 14:39:03 +0330

    3 Base Colors Determined . Primary , Secondary and tertiary .

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./src/App.jsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: { primary: "#0d1b2a", secondary: "#e0e1dd", tertiary: "#778da9" },
+    },
   },
   plugins: [],
 };
