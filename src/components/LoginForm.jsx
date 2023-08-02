9f9ef9776e39a0b8175147094aa90106c8122679
mreza <mrezabmiri17@gmail.com>
2023-08-02 13:17:49 +0330

    reCaptcha added .

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,20 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createRef } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 
 import { Formik, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+
+const TEST_SITE_KEY = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI";
 
 const LoginForm = () => {
   const initialValues = {
     username: "",
     password: "",
+    recaptcha: "",
   };
-
+  const reCaptchaRef = createRef();
   const LoginSchema = Yup.object().shape({
     username: Yup.string().required("نام کاربری را وارد کنید"),
     password: Yup.string().required("رمز ورود را وارد کنید"),
+    recaptcha: Yup.string().required("عدد مقابل را وارد کنید"),
   });
 
   const submitForm = (values) => {
+    reCaptchaRef.current.execute();
     console.log("Form Sub");
   };
   return (
@@ -83,15 +89,26 @@ const LoginForm = () => {
                   </div>
                 )}
               </div>
+              <div className="w-full flex flex-row row-span-1 h-20 justify-center">
+                <ReCAPTCHA
+                  className=" basis-5/12 pl-3 rounded"
+                  sitekey="6Le2nREUAAAAALYuOv7X9Fe3ysDmOmghtj0dbCKW"
+                  render="explicit"
+                  theme="light"
+                />
+              </div>
+              <div className="w-full flex flex-row row-span-1 h-10 justify-center">
+                {errors.recaptcha && touched.recaptcha && (
+                  <div className=" w-5/12 pl-3 text-red-600 float-left inline-block">
+                    {errors.recaptcha}
+                  </div>
+                )}
+              </div>
 
               <div className="w-full flex flex-row row-span-1 h-10 justify-center">
                 <input
                   type="submit"
-                  className={`${
-                    dirty && isValid
-                      ? "bg-green-500 basis-2/12 rounded-md cursor-pointer hover:bg-green-700 hover:basis-3/12"
-                      : "invisible"
-                  } `}
+                  className="bg-green-500 basis-2/12 rounded-md cursor-pointer hover:bg-green-700 hover:basis-3/12"
                   disabled={!(dirty && isValid)}
                   value={"وارد شو"}
                 />
