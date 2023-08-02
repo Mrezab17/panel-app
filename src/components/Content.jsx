2ca217ea3fe7a352d5cac289e9d941fede4b576f
mreza <mrezabmiri17@gmail.com>
2023-08-02 17:22:59 +0330

    On Logout Routing Completed .

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,47 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import { Suspense, lazy, useState, useEffect } from "react";
+
 import Navbar from "./Navbar";
-import Login from "../pages/Login";
+import Loading from "./Loading";
+
+const Login = lazy(() => import("../pages/Login"));
+const Register = lazy(() => import("../pages/Register"));
+const ErrorPage = lazy(() => import("../pages/ErrorPage"));
+const isLogin = false;
 
 const Content = () => {
   return (
-    <div className="w-screen h-screen bg-secondary ">
-      <Navbar />
-      <Login />
-    </div>
+    <Suspense fallback={<Loading />}>
+      <BrowserRouter>
+        <div className="w-screen h-screen bg-secondary ">
+          <Navbar />
+          {isLogin ? (
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={<Navigate to="/login" replace />}
+              />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/register" element={<Register />} />
+              <Route path="/*" element={<ErrorPage error={404} />} />
+            </Routes>
+          ) : (
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={<Navigate to="/login" replace />}
+              />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/register" element={<Register />} />
+              <Route path="/*" element={<ErrorPage error={404} />} />
+            </Routes>
+          )}
+        </div>
+      </BrowserRouter>
+    </Suspense>
   );
 };
 
