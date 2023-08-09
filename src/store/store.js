d4f2fc128496a459de8566a47b86e677c21905dc
mreza <mrezabmiri17@gmail.com>
2023-08-09 20:07:36 +0330

    Presisted Login Completed .

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
 import loginReducer from "./loginSlice";
 import adminsReducer from "./adminsSlice";
 import usersReducer from "./usersSlice";
 
-export default configureStore({
+const persistedLoginReducer = persistReducer(persistConfig, loginReducer);
+
+const store = configureStore({
   reducer: {
-    isLogin: loginReducer,
+    isLogin: persistedLoginReducer,
     users: usersReducer,
     admins: adminsReducer,
   },
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
 });
+export default store;
+export const persistor = persistStore(store);
