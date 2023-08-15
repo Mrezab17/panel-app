42692b906fa7cb79ad70b6e0450472ed0152dd47
mreza <mrezabmiri17@gmail.com>
2023-08-15 20:28:32 +0330

    api Call Seperated from usersActions

--- src/store/usersActions.js
+++ src/store/usersActions.js
@@ -1,22 +1,12 @@
-import { replaceUsers, setFetched, getIsFetched } from "./usersSlice";
+import { replaceUsers, setFetched } from "./usersSlice";
+import { fetchData } from "../api/fetchData";
 
-import axios from "axios";
 const baseUrl = "/users.json";
 
 export const fetchUsersData = () => {
   return async (dispatch) => {
-    const fetchData = async () => {
-      const response = await axios
-        .get(baseUrl)
-        .then((res) => {
-          return res.data;
-        })
-        .catch((err) => console.log(err));
-
-      return response;
-    };
     try {
-      const fetchedUsersList = await fetchData();
+      const fetchedUsersList = await fetchData(baseUrl);
       dispatch(
         replaceUsers({
           items: fetchedUsersList || [],
@@ -25,25 +15,6 @@ export const fetchUsersData = () => {
       dispatch(setFetched());
     } catch (error) {
       console.log("Error Fetching at fetchUsersData" + error);
-    }
-  };
-};
-
-export const sendUsers = (users) => {
-  return async (dispatch, usersList) => {
-    const sendRequest = async () => {
-      const response = await axios.post(baseUrl, usersList).then((response) => {
-        if (!response.ok) {
-          throw new Error("Sending users data failed.");
-        }
-      });
-    };
-
-    try {
-      const usersList = users.items;
-      await sendRequest(usersList);
-    } catch (error) {
-      console.log("Error sending at fetchUsersData : \n" + error);
     }
   };
 };
