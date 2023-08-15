af8f11fbea8039c8fd0c808f5943487d9cd8bfb6
mreza <mrezabmiri17@gmail.com>
2023-08-15 17:00:13 +0330

    Navbar edit styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,17 +15,17 @@ const Navbar = () => {
     <div className="w-screen h-16 sm:h-20 bg-primary py-3 space-x-5 fixed ">
       {(isLogin && (
         <>
-          <div className="text-secondary h-full w-2/12 text-sm sm:text-base px-2 py-2.5 text-center  float-left cursor-none">
+          <div className="text-secondary h-full w-2/12 text-sm sm:text-lg px-2 py-2.5 text-center  float-left cursor-none">
             {""}
             پنل مدیریت {""}
           </div>
-          <div className="text-secondary w-0 sm:w-2/12 h-full sm:text-base px-5 py-2.5 text-center float-right cursor-none hidden sm:block ">
+          <div className="text-secondary w-0 sm:w-2/12 h-full sm:text-lg px-5 py-2.5 text-center float-right cursor-none hidden sm:block ">
             {" "}
             {`${admin.name}  ${admin.lname}  `}{" "}
           </div>
           <button
             type="button"
-            className="text-secondary h-full w-2/12 bg-gradient-to-r from-primary to-tertiary hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-primary  rounded-lg text-base px-5 py-2.5 text-center   float-right"
+            className="text-secondary h-full w-2/12 bg-gradient-to-r from-primary to-tertiary hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-primary  rounded-lg text-lg  px-5 py-2.5 text-center   float-right"
             onClick={() => {
               dispatch(logout());
               navigate("/");
@@ -36,7 +36,7 @@ const Navbar = () => {
           </button>
           <button
             type="button"
-            className="text-secondary h-full  w-3/12 sm:w-2/12 bg-gradient-to-r from-primary to-tertiary hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-primary  rounded-lg text-xs sm:text-base px-5 py-2.5 text-center  float-right"
+            className="text-secondary h-full  w-3/12 sm:w-2/12 bg-gradient-to-r from-primary to-tertiary hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-primary  rounded-lg text-xs sm:text-lg  px-5 py-2.5 text-center  float-right"
             onClick={() => {
               navigate("/editusers");
             }}
@@ -46,7 +46,7 @@ const Navbar = () => {
           </button>
           <button
             type="button"
-            className="text-secondary h-full w-3/12 sm:w-2/12 bg-gradient-to-r from-primary to-tertiary hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-primary  rounded-lg test-xs sm:text-base px-5 py-2.5 text-center mx-2  float-right"
+            className="text-secondary h-full w-3/12 sm:w-2/12 bg-gradient-to-r from-primary to-tertiary hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-primary  rounded-lg test-xs sm:text-lg  px-5 py-2.5 text-center mx-2  float-right"
             onClick={() => {
               navigate("/deleteusers");
             }}
